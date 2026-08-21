b3d3a95b2adac40a2af41b7f90c3868adcbee5a4
loremus <loremus@proton.me>
2026-08-21 13:27:46 +0530

    blahaj responsive

--- app/blahaj/page.tsx
+++ app/blahaj/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
   return (
     <main className="w-full max-w-2xl grid gap-4 p-4 pt-36">
       <div className="text-neutral-100 tracking-tight">Blahaj</div>
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-2 gap-4 portrait:grid-cols-1">
         {images.map((image) => (
           <div key={image} className="grid gap-2">
             <Image
